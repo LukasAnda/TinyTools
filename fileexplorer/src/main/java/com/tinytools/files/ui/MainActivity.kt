ebefaa99b82a9165d0f90aee83add13b20a50390
Lukas Anda <lukas.anda@gmail.com>
2020-09-06 23:00:35 +0200

    Add hamburger menu

--- fileexplorer/src/main/java/com/tinytools/files/ui/MainActivity.kt
+++ fileexplorer/src/main/java/com/tinytools/files/ui/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.tinytools.files.ui
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import com.tinytools.common.activities.ThemedActivity
 import com.tinytools.common.views.DrawerView
+import com.tinytools.files.R
 import com.tinytools.files.databinding.MainActivityBinding
 import com.tinytools.files.ui.files.fragment.FilesFragment
 
 class MainActivity : ThemedActivity<MainActivityBinding>(), DrawerView.DrawerHandler {
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun getViewBinding() = MainActivityBinding.inflate(layoutInflater)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setSupportActionBar(binding.toolbar)
+        toggle = ActionBarDrawerToggle(this, binding.root, R.string.nav_app_bar_open_drawer_description, R.string.nav_app_bar_navigate_up_description)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.run {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeButtonEnabled(true)
+        }
     }
 
     override fun onResume() {
@@ -24,7 +37,23 @@ class MainActivity : ThemedActivity<MainActivityBinding>(), DrawerView.DrawerHan
         swapColors()
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar items
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return false
+    }
+
     override fun onDrawerConfigurationChanged(configuration: DrawerView.Configuration) {
         binding.drawer.reloadConfiguration(configuration.apply { handler = this@MainActivity })
+    }
+
+    override fun onBackPressed() {
+        if (binding.root.isDrawerOpen(GravityCompat.START)) {
+            binding.root.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
